c96db3f9966ce2160b6b79663eb5c6de9b4d21ea
Tyler <16465776+TerabyteTiger@users.noreply.github.com>
2024-09-09 13:28:03 -0400

    😅 minor fixes and whatnot

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,48 @@
 <template>
   <header class="text-4xl font-bold pl-4 py-4">Deck Diff</header>
 
-  <main class="grid grid-cols-1 xl:grid-cols-2">
+  <main class="grid grid-cols-1 xl:grid-cols-2 mb-24">
     <div class="flex flex-col">
       <h2 class="text-center text-xl mb-2">The list you have:</h2>
       <textarea
-      name="list1"
-      id="list1"
-      v-model="list1"
-      rows="25"
-      class="border-2 border-gray-300 p-4 w-3/4 mx-auto"
+        name="list1"
+        id="list1"
+        v-model="list1"
+        rows="25"
+        class="border-2 border-gray-300 p-4 w-3/4 mx-auto"
       ></textarea>
     </div>
     <div class="flex flex-col">
       <h2 class="text-center text-xl mb-2">The list you want:</h2>
       <textarea
-      name="list2"
-      id="list2"
-      v-model="list2"
-      rows="25"
-      class="border-2 border-gray-300 p-4 w-3/4 mx-auto"
+        name="list2"
+        id="list2"
+        v-model="list2"
+        rows="25"
+        class="border-2 border-gray-300 p-4 w-3/4 mx-auto"
       ></textarea>
     </div>
-    <button @click="compare()" class="px-4 py-2 bg-blue-300 border-2 border-blue-800  mx-auto rounded my-8 xl:col-span-2">Compare</button>
+    <button
+      @click="compare()"
+      class="px-4 py-2 bg-blue-300 border-2 border-blue-800 mx-auto rounded my-8 xl:col-span-2"
+    >
+      Compare
+    </button>
     <div class="px-8">
       <h2 class="text-2xl font-bold underline">Remove:</h2>
-      <p class="text-red-800 font-bold text-lg pl-4">
-        <ul class="list-disc list-inside">
-          <li v-for="(card, index) in Object.keys(removeList)" :key="index">{{removeList[card]}} {{ card }}</li>
-        </ul>
-      </p>
+      <ul class="list-disc list-inside text-red-800 font-bold text-lg pl-4">
+        <li v-for="(card, index) in Object.keys(removeList)" :key="index">
+          {{ removeList[card] }} {{ card }}
+        </li>
+      </ul>
     </div>
     <div class="px-8">
       <h2 class="text-2xl font-bold underline">Add:</h2>
-      <p class="text-green-800 font-bold text-lg pl-4">
-        <ul class="list-disc list-inside">
-          <li v-for="(card, index) in Object.keys(addList)" :key="index">{{addList[card]}} {{ card }}</li>
-        </ul>
-      </p>
+      <ul class="list-disc list-inside text-green-800 font-bold text-lg pl-4">
+        <li v-for="(card, index) in Object.keys(addList)" :key="index">
+          {{ addList[card] }} {{ card }}
+        </li>
+      </ul>
     </div>
   </main>
 </template>
